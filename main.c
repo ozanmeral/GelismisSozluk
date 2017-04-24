c36b18ffb8d1ef3db888257aa9ea7e2c54390a40
Ömrüm Baki Temiz <omrumbakitemiz@icloud.com>
2017-04-24 21:03:27 +0300

    func2 eklendi

--- main.c
+++ main.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 
 void func();
+void func2();
 
 int main(){
 	printf("Hello World");
 	func();
+	func2();
 }
 
 void func(){
+	printf("sa as");
+}
+
+void func2(){
 	printf("sa as");
 }
